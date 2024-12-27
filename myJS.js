076be9028e02a33a4153abc768f061c6cb602ec3
AminDantey <143043125+AminDantey@users.noreply.github.com>
2024-12-27 11:16:31 +0330

    Add files via upload

---
+++ myJS.js
@@ -0,0 +1,22 @@
+  alert("Hello again ...")
+var Obj1 = {
+    "student1" : "Amin Hajian",
+    "student2" : "Imanoy madaeni",
+     const : "camelCase"
+}
+let Obj2 = {
+    "NumbS1": 937,
+    "NumbS2":913,
+    int : 999
+};
+console.log(Obj1);
+console.log(Obj2.int)
+console.log(Obj2.NumbS1)
+
+ const Score = 2000;
+ function showMessage(){
+     let x1="welcome to this page.";
+     let x2=" this is your Score Amout:[ "+ Score+" ]^^";
+     const msg = x1+x2;
+     document.getElementById("x2").innerText = msg+" thanks for visiting us!";
+ }
